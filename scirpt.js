8b8adc014c3812d4cf91ac9c86670221e1af920f
mohit shinde <mohitshinde1111111111@gmail.com>
2026-01-08 16:31:33 +0530

    yes
    yes

---
+++ scirpt.js
@@ -0,0 +1,15 @@
+    const bar = document.getElementById("bar");
+    const navbar = document.getElementById("navbar");
+
+    if (bar) {
+        bar.addEventListener("click", () => {
+            navbar.classList.toggle("active");
+        });
+    }
+
+    if (close) {
+        close.addEventListener("click", () => {
+            navbar.classList.remove("active");
+        });
+    }
+
